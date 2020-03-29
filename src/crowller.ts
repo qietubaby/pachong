0ddca6b311ff2461c542859f303d2c8902a945dd
qietubaby <201951244@qq.com>
2020-03-29 18:59:46 +0800

    使用cheerio进行数据提取

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,14 +1,45 @@
 // superagent 获取 html 内容
 import superagent from 'superagent'
 
+import cheerio from 'cheerio'
+
+interface Course {
+  title: string
+  type: string
+}
+
 class Crowller {
-  private secret = 'secretKey'
-  private url = `https://www.yanyue.cn/tobacco`
+  private url = `http://www.kdxs.com/`
   private rawHtml = ''
+
+  getJsonInfo(html: string) {
+    const $ = cheerio.load(html)
+    const courseItems = $('.pt-new-recomment-list li')
+    const courseInfos: Course[] = []
+
+    courseItems.map((index, element) => {
+      const descs = $(element).find('a')
+      const title = descs.eq(1).text()
+      const type = descs.eq(0).text()
+
+      courseInfos.push({
+        title,
+        type
+      })
+    })
+
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
+
+    console.log(result)
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
     this.rawHtml = result.text
+    this.getJsonInfo(this.rawHtml)
   }
 
   constructor() {
